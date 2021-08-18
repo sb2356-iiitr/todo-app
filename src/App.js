84be0848b7f9ab467f3ab3cc81a83885a18da136
sb2356-iiitr <soumyabrata.btech.cs19@iiitranchi.ac.in>
2021-08-18 13:20:14 +0400

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
+  const arr = [
+    {
+      id: 1,
+      name: 'a',
+    },
+    {
+      id: 2,
+      name: 'b',
+    },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>ToDo List App</h1>
+        <form className="todoForm">
+          <input type="text" />
+          <button>Go</button>
+        </form>
+        {/* For list of events */}
+        <ul>
+          <li>
+            <span>Learn React</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+          <li>
+            <span>Learn React</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
