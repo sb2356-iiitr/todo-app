c8975f891f604f3578e2bc1e7d558b732c25f146
sb2356-iiitr <soumyabrata.btech.cs19@iiitranchi.ac.in>
2021-08-19 09:24:53 +0400

    Completed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import "./App.css"
 const App = () => {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
+  const [editId, setEditId] = useState(0)
 
   const handleSubmit = (e) => {
     /* prevent refreshing of page */
     e.preventDefault();
 
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId)
+      const updatedTodos = todos.map((t)=>t.id === editTodo.id 
+      ? (t = {id: t.id, todo})
+      : {id: t.id, todo:t.todo}
+      )
+      setTodos(updatedTodos)
+      setEditId(0)
+      setTodo("")
+      return
+    }
+
     if (todo !== "") {
       /*... (spread operator), appends to the list */
       setTodos([{id:`${todo}-${Date.now()}`, todo}, ...todos])
+      setTodo("")
     }
+  }
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id)
+    setTodos([...delTodo])
+  }
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id)
+    setTodo(editTodo.todo)
+    setEditId(id)
   }
 
   return (
@@ -20,16 +45,20 @@ const App = () => {
       <div className="container">
         <h1>ToDo List App</h1>
         <form className="todoForm" onSubmit={handleSubmit}>
-          <input type="text" onChange={(e) => setTodo(e.target.value)}/>
-          <button type="submit">Go</button>
+          <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)}/>
+          <button type="submit">{editId?"Edit":"Go"}</button>
         </form>
         {/* For list of events */}
         <ul className="allTodos">
-          <li className="singleTodo">
-            <span className="todoText">Learn React</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
+          {
+            todos.map((t)=>(
+              <li className="singleTodo">
+                <span className="todoText" key={t.id}>{t.todo}</span>
+                <button onClick={() => handleEdit(t.id)}>Edit</button>
+                <button onClick={() => handleDelete(t.id)}>Delete</button>
+              </li>
+            ))
+          }
         </ul>
       </div>
     </div>
